064a372554753dc9f2c69100e36ba4769f6c207c
kaveen <kaveensmart28@gmail.com>
2026-03-19 17:40:55 +0530

    coded UC3

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,6 +1,12 @@
 public class HelloApp{
     public static void main(String[] args){
-        String name = args[0];
-        System.out.println("Hello, "+ name + "!");
+        String defname = "World";
+        if(args.length > 0){
+            String name = args[0];
+            System.out.println("Hello, "+ name + "!");
+            return;
+        }
+        System.out.println("Hello, "+ defname + "!");
+
     }
 }
