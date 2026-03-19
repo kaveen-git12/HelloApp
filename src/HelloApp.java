798ef53f118ce7453070b89c64f765a0b0243c49
kaveen <kaveensmart28@gmail.com>
2026-03-19 18:14:47 +0530

    coded UC4

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,6 +1,28 @@
+<<<<<<< feature/UC2
 public class HelloApp{
     public static void main(String[] args){
         String name = args[0];
         System.out.println("Hello, "+ name + "!");
+=======
+public class HelloApp {
+    public static void main(String[] args) {
+        String defname = "World";
+
+        if (args.length > 0) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                sb.append(args[i]);
+
+                if (i < args.length - 1) {   // we only need to add comma between items
+                    sb.append(", ");
+                }
+            }
+
+            System.out.println("Hello, " + sb + "!");
+        } else {
+            System.out.println("Hello, " + defname + "!");
+        }
+>>>>>>> local
     }
 }
